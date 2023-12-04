714c6f9ecaa4e1f5b658f052da9f3bd74191c7f9
Kalyan Raja <67988405+Kalyanraja@users.noreply.github.com>
2023-12-04 09:53:43 -0500

    Add files via upload

---
+++ KPCoreAlgorithm.java
@@ -0,0 +1,176 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class KPCoreAlgorithm {
+    public static void main(String[] args) {
+        try {
+            // Read input from the layers file
+            ArrayList<int[][]> M = readLayers("layers.txt");
+
+            // Read input from the k_values file
+            int[] k = readArray("k_values.txt");
+
+            // Read input from the p_values file
+            double[] p = readArrayDouble("p_values.txt");
+
+            // Initialize the vertex set Ql
+            Set<Integer> Ql = new HashSet<>();
+            for (int vertex = 0; vertex < M.get(M.size() - 1).length; vertex++) {
+                Ql.add(vertex);
+            }
+
+            // Repeat the vertex peeling process
+            while (true) {
+                // Save a copy of Ql
+                Set<Integer> Qbefore = new HashSet<>(Ql);
+
+                // Peel the vertices with degrees less than k[l]
+                Ql = peelDegree(Ql, M.get(M.size() - 1), k[k.length - 1]);
+
+                // For each other layer, peel the vertices with neighbor coverage fractions less than p[i]
+                for (int i = k.length - 2; i >= 0; i--) {
+                    Ql = peelNeighborCoverage(Ql, M.get(i), p[i]);
+                }
+
+                // Check if Ql has changed
+                if (Ql.equals(Qbefore)) {
+                    break;
+                }
+            }
+
+            // Output the (k, p)-core
+            System.out.println("The (k, p)-core in the graph is: " + Ql);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+            e.printStackTrace();
+        } catch (NoSuchElementException | IllegalStateException e) {
+            System.err.println("Input error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<int[][]> readLayers(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+
+        // Check if there is at least one layer
+        if (!scanner.hasNextInt()) {
+            throw new NoSuchElementException("The layers file is empty or does not contain the expected data.");
+        }
+
+        int numLayers = scanner.nextInt();
+        ArrayList<int[][]> layers = new ArrayList<>();
+
+        for (int layer = 0; layer < numLayers; layer++) {
+            // Check if there are enough values for the current layer
+            if (!scanner.hasNextInt() || !scanner.hasNextInt()) {
+                throw new NoSuchElementException("Invalid format in the layers file.");
+            }
+
+            int numVertices = scanner.nextInt();
+            int numEdges = scanner.nextInt();
+            int[][] currentLayer = new int[numVertices][numEdges];
+
+            // Check if there are enough values for the edges
+            for (int vertex = 0; vertex < numVertices; vertex++) {
+                for (int edge = 0; edge < numEdges; edge++) {
+                    // Check if there are enough values for the edge
+                    if (!scanner.hasNextInt()) {
+                        throw new NoSuchElementException("Invalid format in the layers file.");
+                    }
+                    currentLayer[vertex][edge] = scanner.nextInt();
+                }
+            }
+
+            layers.add(currentLayer);
+        }
+        scanner.close();
+        return layers;
+    }
+
+    private static int[] readArray(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+
+        // Check if there is at least one element
+        if (!scanner.hasNextInt()) {
+            throw new NoSuchElementException("The array file is empty or does not contain the expected data.");
+        }
+
+        int numElements = scanner.nextInt();
+        int[] array = new int[numElements];
+
+        // Check if there are enough values for the array
+        for (int i = 0; i < numElements; i++) {
+            // Check if there are enough values for the array
+            if (!scanner.hasNextInt()) {
+                throw new NoSuchElementException("Invalid format in the array file.");
+            }
+            array[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        return array;
+    }
+
+    private static double[] readArrayDouble(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+
+        // Check if there is at least one element
+        if (!scanner.hasNextInt()) {
+            throw new NoSuchElementException("The array file is empty or does not contain the expected data.");
+        }
+
+        int numElements = scanner.nextInt();
+        double[] array = new double[numElements];
+
+        // Check if there are enough values for the array
+        for (int i = 0; i < numElements; i++) {
+            // Check if there are enough values for the array
+            if (!scanner.hasNextDouble()) {
+                throw new NoSuchElementException("Invalid format in the array file.");
+            }
+            array[i] = scanner.nextDouble();
+        }
+
+        scanner.close();
+        return array;
+    }
+
+    private static Set<Integer> peelDegree(Set<Integer> Ql, int[][] M, int k) {
+        Set<Integer> toRemove = new HashSet<>();
+
+        for (int vertex : Ql) {
+            if (getDegree(vertex, M) < k) {
+                toRemove.add(vertex);
+            }
+        }
+
+        Ql.removeAll(toRemove);
+        return Ql;
+    }
+
+    private static Set<Integer> peelNeighborCoverage(Set<Integer> Ql, int[][] M, double p) {
+        Set<Integer> toRemove = new HashSet<>();
+
+        for (int vertex : Ql) {
+            double neighborCoverageFraction = computeNeighborCoverageFraction(vertex, M);
+            if (neighborCoverageFraction < p) {
+                toRemove.add(vertex);
+            }
+        }
+
+        Ql.removeAll(toRemove);
+        return Ql;
+    }
+
+    private static int getDegree(int vertex, int[][] M) {
+        return M[vertex].length; // Assumes undirected graph
+    }
+
+    private static double computeNeighborCoverageFraction(int vertex, int[][] M) {
+        int neighborCount = M[vertex].length;
+        double neighborCoverageFraction = (double) neighborCount / (M.length - 1); // -1 to exclude self-loop
+        return neighborCoverageFraction;
+    }
+}
---
+++ KPPTreeAlgorithm.java
@@ -0,0 +1,204 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class KPPTreeAlgorithm {
+    static class Graph {
+        private List<Set<Integer>>[] layers;
+
+        public Graph(int numLayers) {
+            layers = new ArrayList[numLayers];
+            for (int i = 0; i < numLayers; i++) {
+                layers[i] = new ArrayList<>();
+            }
+        }
+
+        public void addEdges(int layer, int[][] edges) {
+            Set<Integer> layerEdges = new HashSet<>();
+            for (int[] edge : edges) {
+                layerEdges.add(edge[0]);
+                layerEdges.add(edge[1]);
+            }
+            layers[layer].add(layerEdges);
+        }
+
+        public int getNumLayers() {
+            return layers.length;
+        }
+
+        
+
+        public int getLayerDegree(int layer) {
+            
+            return 0; 
+        }
+    }
+
+    static class KPTree {
+        static class Node {
+            int[] kVec;
+            PTree pTree;
+            Node nextNode;
+
+            public Node(int[] kVec, PTree pTree) {
+                this.kVec = kVec;
+                this.pTree = pTree;
+                this.nextNode = null;
+            }
+        }
+
+        private Node root;
+
+        public KPTree() {
+            root = null;
+        }
+
+        public void insertNode(int[] kVec, PTree pTree) {
+            Node newNode = new Node(kVec, pTree);
+
+            if (root == null) {
+                root = newNode;
+            } else {
+                newNode.nextNode = root;
+                root = newNode;
+            }
+        }
+
+    }
+
+    static class PTree {
+        // Placeholder for PTree related functionalities
+        // ...
+    }
+
+    public static Set<int[]> GCDPlus(Graph graph) {
+        Set<int[]> result = new HashSet<>();
+        KPTree kptree = new KPTree();
+        try {
+            KPTreeDFS(graph, kptree, result);
+        } catch (InputMismatchException e) {
+            System.err.println("Invalid format in the input file.");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void PTreeDFS(Graph graph, KPTree kptree, Set<int[]> result) {
+        Set<Integer>[] cores = new HashSet[graph.getNumLayers()];
+        for (int i = 0; i < cores.length; i++) {
+            cores[i] = new HashSet<>();
+            // Add some sample data to the sets for demonstration
+            cores[i].add(i + 1); // Add example element
+        }
+
+        int[] k = new int[graph.getNumLayers()]; // Placeholder variables for demonstration
+        double[] p = new double[graph.getNumLayers()]; // Placeholder variables for demonstration
+
+        Set<Integer> Ql = cores[cores.length - 1]; // Assume Ql is obtained
+
+        if (!Ql.isEmpty()) {
+            result.add(new int[]{1, 2, 3}); // Add (Ql, k, p) to result (just for demonstration)
+            for (int i = cores.length - 2; i >= 0; i--) {
+                if (i >= 0 && p[i] < cores[i].size()) {
+                    double[] pPrime = toFrac(p.clone());
+                    pPrime[i] += 1; // Update pPrime
+
+                    // PTreeDFS recursive call on subgraph Mk[Q]
+                    try {
+                        PTreeDFS(graph, kptree, result); // Fix the recursive call arguments
+                    } catch (InputMismatchException e) {
+                        System.err.println("Invalid format in the input file.");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void KPTreeDFS(Graph graph, KPTree kptree, Set<int[]> result) {
+        int[][] cores = new int[graph.getNumLayers()][]; // Placeholder for cores
+        for (int i = 0; i < cores.length; i++) {
+            cores[i] = new int[]{i + 1}; // Sample data for demonstration
+        }
+
+        int[] k = new int[graph.getNumLayers()]; // Placeholder variables for demonstration
+
+        int[] Ql = cores[cores.length - 1]; // Assume Ql is obtained
+
+        if (Ql.length > 0) {
+            result.add(Ql); // Add (Ql, k, p) to result (just for demonstration)
+            for (int i = cores.length - 1; i >= 0; i--) {
+                if (i >= 0 && k[i] < graph.getLayerDegree(i)) {
+                    int[] kPrime = k.clone();
+                    kPrime[i] += 1; // Update kPrime
+
+                    // KPTreeDFS recursive call on subgraph Mk[Q]
+                    try {
+                        KPTreeDFS(graph, kptree, result); // Fix the recursive call arguments
+                    } catch (InputMismatchException e) {
+                        System.err.println("Invalid format in the input file.");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static double[] toFrac(double[] p) {
+        // Placeholder for converting p to its fractional form as described in Algorithm 2
+        // Implementation needs to convert p to the fractional form (e.g., using specific calculations)
+        // Return the converted fractional form of p
+        return new double[]{1.0, 0.5, 0.25}; // Placeholder value for demonstration
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("dataset.txt"));
+
+            // Read the number of layers
+            if (!scanner.hasNextInt()) {
+                throw new InputMismatchException("Invalid format in the input file.");
+            }
+            int numLayers = scanner.nextInt();
+            Graph graph = new Graph(numLayers);
+
+            // Read edges for each layer
+            for (int i = 0; i < numLayers; i++) {
+                if (!scanner.hasNextInt()) {
+                    throw new InputMismatchException("Invalid format in the input file.");
+                }
+                int numEdges = scanner.nextInt();
+                int[][] edges = new int[numEdges][2];
+
+                for (int j = 0; j < numEdges; j++) {
+                    if (!scanner.hasNextInt()) {
+                        throw new InputMismatchException("Invalid format in the input file.");
+                    }
+                    edges[j][0] = scanner.nextInt();
+                    if (!scanner.hasNextInt()) {
+                        throw new InputMismatchException("Invalid format in the input file.");
+                    }
+                    edges[j][1] = scanner.nextInt();
+                }
+
+                graph.addEdges(i, edges);
+            }
+
+            scanner.close();
+
+            // Perform GCD+ Algorithm
+            Set<int[]> result = GCDPlus(graph);
+
+            // Display the result
+            for (int[] core : result) {
+                System.out.println("Core: " + Arrays.toString(core));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+            e.printStackTrace();
+        } catch (InputMismatchException e) {
+            System.err.println("Input error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
